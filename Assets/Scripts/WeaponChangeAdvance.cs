4c96fcec2836efb63bf27a225219e888f7ab1e0d
MehboobAli-Portfolio <mehboob56ali78@gmail.com>
2025-09-25 18:46:14 +0500

    Add aim setup and camera logic to WeaponChangeAdvance
    Enhanced WeaponChangeAdvance to support MultiAimConstraint setup and camera assignment for local player using Cinemachine and Photon. Updated YBot prefab to include aimObjects and adjusted active states. Added a new _Recovery scene and associated meta files.

--- Assets/Scripts/WeaponChangeAdvance.cs
+++ Assets/Scripts/WeaponChangeAdvance.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using System.Collections;  
 using UnityEngine.Animations.Rigging;
-
+using Cinemachine;
+using Photon.Pun;
 public class WeaponChangeAdvance : MonoBehaviour
 {
     public TwoBoneIKConstraint leftHand;
     public TwoBoneIKConstraint rightHand;
     public TwoBoneIKConstraint leftHandThumb;
+
+    private CinemachineVirtualCamera cam;
+    private GameObject camObject;
+
+    public MultiAimConstraint[] aimObjects;
+    private Transform aimTarget;
+
     public RigBuilder rig;
     public Transform[] leftTargets;
     public Transform[] rightTargets;
@@ -17,7 +25,29 @@ public class WeaponChangeAdvance : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        camObject = GameObject.Find("PlayerCam");
+        aimTarget = GameObject.Find("AimRef").transform;
+        if (this.gameObject.GetComponent<PhotonView>().IsMine)
+        {
+            cam = camObject.GetComponent<CinemachineVirtualCamera>();
+            cam.Follow = this.gameObject.transform;
+            cam.LookAt = this.gameObject.transform;
+            Invoke("SetLookAt", 0.1f);
+        }
         
+    }
+    void SetLookAt()
+    {
+        if(cam != null)
+        {
+            for(int i=0; i < aimObjects.Length ; i++)
+            {
+                var target = aimObjects[i].data.sourceObjects;
+                target.SetTransform(0, aimTarget);
+                aimObjects[i].data.sourceObjects=target;
+            }
+            rig.Build();
+        }
     }
 
     // Update is called once per frame
